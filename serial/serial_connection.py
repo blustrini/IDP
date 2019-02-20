52dca65afd932746a22587b00cd6b689211df589
Bernardo Lustrini <bl434@cam.ac.uk>
2019-02-20 10:36:39 +0000

    small

--- serial/serial_connection.py
+++ serial/serial_connection.py
@@ -2,13 +2,32 @@ import serial
 from subprocess import *
 import matplotlib.pyplot as plt
 import time
+import glob
 
-board = serial.Serial("/dev/tty.usbmodem144101", 9600)
-# This creates an object able to establish a serial communication channel
-# with the board. The first parameter depends on your operating system
-# and probably needs to be updated.
-# The second is the baud rate. It needs to match the board's settings.
 
+def find_usb_port():
+    #gets list of ports
+    ports = glob.glob('/dev/tty.*')
+
+    #finds usb port
+    for port_test in ports:
+    	if 'usb' in port_test:
+    		return port_test
+    
+    return 0
+
+def establish_serial(port):
+    #create serial connection to board
+    try:
+    	board = serial.Serial(port,9600)
+    #raise error if no usb port is found
+    except ValueError:
+        print('ValueError: No USB port found')
+        return 0
+    except OSError:
+        print('OSError: Couldn\'t connect to port')
+        return 0
+    return 1
 
 while True:
     board.write(b'led_on')
