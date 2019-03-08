52ca167d09f9d098a97d9d6f51a145710745a2e3
Ong Ray Yi <ryo20@cam.ac.uk>
2019-03-08 14:59:29 +0000

    hall detect and ir tests

---
+++ robot/tests/halldetecttest/halldetecttest.ino
@@ -0,0 +1,14 @@
+//int hallpin = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(1000);
+  pinMode(A0, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorvalue = analogRead(A0);
+  Serial.println(sensorvalue);
+}
